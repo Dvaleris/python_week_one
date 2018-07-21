b554692665426b903c9c646854f01f0673f58b86
Dvaleris <41225899+Dvaleris@users.noreply.github.com>
2018-07-21 19:07:54 -0400

    Add files via upload
    Practice file for first day's lesson

---
+++ calc.py
@@ -0,0 +1,56 @@
+# calculator
+
+print(1+2)
+print(3)
+
+print(10%2)
+print(11%2)
+
+for i in range(0,9):
+    print("bitshift ", i, "times ", 1<<i)
+
+print('Hello, world!')
+print('')
+print('Good-bye.')
+
+print( 'I like' + 'chocolate cake.' )
+print('I like ' + 'apple pie.')
+print('I like' + ' apple pie.')
+
+print('blink ' * 4)
+print('moo'*3)
+# print('moo'/3) Invalid string manip
+
+print(12 + 12)
+print('12' + '12')
+print('12 + 12')
+
+print(2 * 5)
+print('2' * 5)
+print('2 * 5')
+
+# print('12' + 12) Cannot concat string + int
+print('12' + str(12))
+
+# print('2' * '5') Cannot multiply strings
+
+#print('Betty' + 12) See Line 32
+#print('Francesca' * 'Hanna') saa
+
+print('You\'re swell!')
+print('backslash at the end of a string: \\')
+print('up\\down')
+print('up\down')
+
+print(1 + 2)
+print(2 + 3)
+print(5 - 8)
+print(9 / 2)
+
+print(5 * (12 - 8) + -15)
+print(98 + (59872 / (13 * 8)) * -51)
+
+print(10 % 2)
+
+print(2**8)
+print(1<<9)
